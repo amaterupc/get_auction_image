252ac781449626e17415de75587eb077267ff7e4
あまてる <amateru.ig@icloud.com>
2025-12-18 19:07:03 +0900

    Initial version: basic image downloader

---
+++ get_auction_image.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+import os
+
+# URL of the page
+url = "https://page.auctions.yahoo.co.jp/jp/auction/o1146778518"
+
+# Send a GET request to the page
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find all images with the specified class
+img_tags = soup.find_all('img', class_='ProductImage__image')
+
+# Create a directory to save images
+if not os.path.exists('yahoo_images'):
+    os.makedirs('yahoo_images')
+
+# Download and save each image
+for img in img_tags:
+    img_url = img.get('src')
+    if img_url:
+        # Convert relative URLs to absolute URLs
+        img_url = urllib.parse.urljoin(url, img_url)
+        # Get the image filename
+        img_name = os.path.join('yahoo_images', os.path.basename(img_url))
+        # Download and save the image
+        with open(img_name, 'wb') as f:
+            f.write(requests.get(img_url).content)
+        print(f'Downloaded: {img_name}')
+
+print("Download complete!")
+
