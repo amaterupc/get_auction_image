58732da4032940520130be6e5731e451eb2307c4
あまてる <amateru.ig@icloud.com>
2025-12-18 19:08:33 +0900

    Version 3: Switched to li tag selection for better reliability

--- get_auction_image.py
+++ get_auction_image.py
@@ -6,27 +6,22 @@ import os
 # URL of the page
 url = "https://page.auctions.yahoo.co.jp/jp/auction/o1146778518"
 
-print(f"Fetching page: {url}")
 # Send a GET request to the page
 response = requests.get(url)
-print(f"Response status code: {response.status_code}")
-
-# Parse the page content
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find all images with the specified class
-img_tags = soup.find_all('img', class_='ProductImage__image is-off')
-print(f"Found {len(img_tags)} images with class 'ProductImage__image'")
+# Find all li tags with class 'ProductImage__image'
+li_tags = soup.find_all('li', class_='ProductImage__image')
 
 # Create a directory to save images
 if not os.path.exists('yahoo_images'):
     os.makedirs('yahoo_images')
-    print("Created directory 'yahoo_images'")
 
 # Download and save each image
-for img in img_tags:
-    img_url = img.get('src')
-    if img_url:
+for li in li_tags:
+    img = li.find('img')
+    if img and img.get('src'):
+        img_url = img.get('src')
         # Convert relative URLs to absolute URLs
         img_url = urllib.parse.urljoin(url, img_url)
         print(f"Downloading image: {img_url}")
